c093670bad21d38ca1d0cddca708d4f3588450f7
Sujal Bhavsar <sujalofficial011@gmail.com>
2024-12-10 00:17:15 +0530

    mdx styles updated

---
+++ app/(slashLayout)/layout.tsx
@@ -0,0 +1,29 @@
+import Footer from "@/app/components/Footer";
+import Header from "@/app/components/Header";
+import type { Metadata } from "next";
+import './mdx-styles.css'
+
+export const metadata : Metadata= {
+  title: 'Projects | Sujal Bhavsar',
+  description: 'Projects of Sujal Bhavsar, a full stack developer from India passionate about crafting solutions and building the future.',
+}
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <header className='fixed top-0 left-0 right-0 bg-white/80 backdrop-blur-sm z-50'>
+      <Header/>
+        </header>
+      <main className="px-[20%] bg-gray-50 max-sm:px-[10%] max-md:px-[10%] max-lg:px-[15%] pt-16">
+        
+        {children}
+      </main>
+    <footer className="bg-gray-900 text-white py-12">
+      <Footer/>
+      </footer>
+    </div>
+  );
+}
