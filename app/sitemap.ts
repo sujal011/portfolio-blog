89850f935ba88762c10c5361d58b64a17a7d99de
Sujal Bhavsar <sujalofficial011@gmail.com>
2024-12-11 22:28:07 +0530

    sitemap added

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.sujalbhavsar.in',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.sujalbhavsar.in/project/ai-powered-whiteboard',
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.sujalbhavsar.in/blog/first-blog',
+      lastModified: new Date(),
+      priority: 0.5,
+    },
+  ]
+}
