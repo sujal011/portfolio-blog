b6a3378b36b5dfcfcd048146682c9e8a8271fbbb
Sujal Bhavsar <sujalofficial011@gmail.com>
2025-02-01 09:07:56 +0530

    diff branch

--- app/(slashLayout)/layout.tsx
+++ app/(slashLayout)/layout.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/app/components/Footer";
 import Header from "@/app/components/Header";
 import type { Metadata } from "next";
-import './mdx-styles.css'
+// import './mdx-styles.css'
 
 export const metadata : Metadata= {
   title: 'Projects | Sujal Bhavsar',
@@ -17,8 +17,10 @@ export default function RootLayout({
       <header className='fixed top-0 left-0 right-0 bg-white/80 backdrop-blur-sm z-50'>
       <Header/>
         </header>
-      <main className="px-[20%] bg-gray-50 max-sm:px-[10%] max-md:px-[10%] max-lg:px-[15%] pt-16 mdx-main">
+      <main className="px-[20%] bg-gray-50 max-sm:px-[10%] max-md:px-[10%] max-lg:px-[15%] pt-16 text-black">
+        <br />
         {children}
+        <br />
       </main>
     <footer className="bg-gray-900 text-white py-12">
       <Footer/>
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  pageExtensions:['mdx','ts','tsx'],
-  experimental:{
-    mdxRs:true,
-  }
 };
 
-const withMDX = createMDX({});
-
-export default withMDX(nextConfig);
+export default nextConfig;
